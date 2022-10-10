2bbfd64de558580423cb1af031172acb4c5b6e4c
Michael Hart <michaelhart@michaelhart.me>
2022-10-10 15:47:30 -0700

    sample route and node version selection for cloudflare

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChargeComponent } from './charge/charge.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'charge', pathMatch: 'full' },
+  { path: 'charge', component: ChargeComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: false,
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChargeComponent } from './charge/charge.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChargeComponent,
   ],
   imports: [
     BrowserModule,
